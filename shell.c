005fb7d808921a1a15545801ffd7243e55dce0ce
sumin3 <suminyu@yahoo.com>
2018-08-20 22:24:10 +0000

    use continue instead of use if statement when we want to skip some instructions

--- shell.c
+++ shell.c
@@ -28,57 +28,54 @@ int main(int argc __attribute__((unused)), char **argv, char **env)
 		{
 			break;
 		}
-		if (buff && buff[0] != '\n')
+		if (buff && buff[0] == '\n')
+			continue;
+		buff_tk = create_arg_list(buff_tk, buff, " \n");
+		if (get_builtin_func(buff_tk)(buff_tk, env, buff) == 0)
+			continue;
+		check_path = access(buff_tk[0], X_OK);
+		if (check_path == -1)
 		{
-			buff_tk = create_arg_list(buff_tk, buff, " \n");
-			if (get_builtin_func(buff_tk)(buff_tk, env, buff))
+			path = _getenv("PATH", env);
+			buff_tk1 = _strdup(buff_tk[0]);
+			buff_tk[0] = path_helper(path, buff_tk[0]);
+		}
+		if (buff_tk[0] == NULL)
+		{
+			write(STDOUT_FILENO, argv[0], _strlen(argv[0]));
+			write(STDOUT_FILENO, ": ", 2);
+			write(STDOUT_FILENO, num_to_str(input_count), _strlen(num_to_str(input_count)));
+			write(STDOUT_FILENO, ": ", 2);
+			write(STDOUT_FILENO, buff_tk1, _strlen(buff_tk1));
+			write(STDOUT_FILENO, ": not found\n", 12);
+			continue;
+		}
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror(argv[0]);
+		}
+		if (child_pid == 0)
+		{
+			if (execve(buff_tk[0], buff_tk, NULL) == -1)
 			{
-				check_path = access(buff_tk[0], X_OK);
-				if (check_path == -1)
-				{
-					path = _getenv("PATH", env);
-					buff_tk1 = _strdup(buff_tk[0]);
-					buff_tk[0] = path_helper(path, buff_tk[0]);
-				}
-				if (buff_tk[0] == NULL)
-				{
-					write(STDOUT_FILENO, argv[0], _strlen(argv[0]));
-					write(STDOUT_FILENO, ": ", 2);
-					write(STDOUT_FILENO, num_to_str(input_count), _strlen(num_to_str(input_count)));
-					write(STDOUT_FILENO, ": ", 2);
-					write(STDOUT_FILENO, buff_tk1, _strlen(buff_tk1));
-					write(STDOUT_FILENO, ": not found\n", 12);
-					continue;
-				}
-				child_pid = fork();
-				if (child_pid == -1)
-				{
-					perror(argv[0]);
-				}
-				if (child_pid == 0)
-				{
-					if (execve(buff_tk[0], buff_tk, NULL) == -1)
-					{
-						perror(argv[0]);
-						free(buff_tk);
-						free(buff);
-						break;
-					}
-					
-				}
-				else
-				{
-					wait(&stat);
-					if (check_path == -1)
-						free(buff_tk[0]);
-					free(buff_tk);
-					free(buff);				
-					buff_tk = NULL;
-					buff = NULL;
-					path = NULL;	
-				}
-				
+				perror(argv[0]);
+				free(buff_tk);
+				free(buff);
+				break;
 			}
+			
+		}
+		else
+		{
+			wait(&stat);
+			if (check_path == -1)
+				free(buff_tk[0]);
+			free(buff_tk);
+			free(buff);				
+			buff_tk = NULL;
+			buff = NULL;
+			path = NULL;	
 		}
 	}
 	return (0);
