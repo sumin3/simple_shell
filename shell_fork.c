4c0b7b90020051a35be1f293e95ff2736f4b1fc9
sumin3 <suminyu@yahoo.com>
2018-08-29 00:26:37 +0000

    set buff_tk to NULL

--- shell_fork.c
+++ shell_fork.c
@@ -39,8 +39,10 @@ void exec_command(char *argv, char **buff_tk,
 			/*free(buff_tk[0]);*/
 			free(buff_tk1);
 			buff_tk1 = NULL;
+			
 		}
 		free(buff_tk);
+		buff_tk = NULL;
 		/*free(buff);*/
 	}
 }
